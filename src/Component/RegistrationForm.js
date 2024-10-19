e196811548b538cf3ab716c50c6c813631910ae7
Reagan Mariera <162294868+Reagan-marera@users.noreply.github.com>
2024-10-19 13:46:48 +0300

    Update RegistrationForm.js

--- src/Component/RegistrationForm.js
+++ src/Component/RegistrationForm.js
@@ -58,7 +58,7 @@ const RegistrationForm = ({ onRegistrationSuccess }) => {
         }
 
         try {
-            const response = await fetch('http://localhost:5000/register', {
+            const response = await fetch('https://lifestyle.boogiecoin.com/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
